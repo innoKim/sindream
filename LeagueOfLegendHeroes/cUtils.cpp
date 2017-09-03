37bdf640b5a64aea0e517f5963656729dd8c6a82
Mavan <volteck@naver.com>
2017-09-03 17:41:11 +0900

    Util GetAngle 구현

--- LeagueOfLegendHeroes/cUtils.cpp
+++ LeagueOfLegendHeroes/cUtils.cpp
@@ -5,11 +5,21 @@ namespace MY_UTIL
 {
 	float MY_UTIL::GetDistance(float startX, float startY, float endX, float endY)
 	{
-		return 0.0f;
+		float x = endX - startX;
+		float y = endY - endY;
+		return sqrt(x * x + y * y);
 	}
 
 	float MY_UTIL::GetAngle(float startX, float startY, float endX, float endY)
+
 	{
-		return 0.0f;
+		float x = endX - startX;
+		float y = endY - startY;
+		float d = sqrt(x * x + y * y);
+		float angle = acos(x / d);
+
+		if (y > 0) angle = 2 * D3DX_PI - angle;
+		return angle;
 	}
+
 }
--- LeagueOfLegendHeroes/cUtils.h
+++ LeagueOfLegendHeroes/cUtils.h
@@ -4,12 +4,7 @@
 namespace MY_UTIL
 {
 	// 거리 구한다
-	float GetDistance(float startX, float startY, float endX, float endY)
-	{
-		float x = endX - startX;
-		float y = endY - endY;
-		return sqrt(x * x + y * y);
-	}
+	float GetDistance(float startX, float startY, float endX, float endY);
 
 	// 두 지점의 각을 구한다
 	float GetAngle(float startX, float startY, float endX, float endY);
