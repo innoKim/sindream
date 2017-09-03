51530efd324c83c7b73aba5aa463c3f8ff0488f4
innoKim <idhik@naver.com>
2017-09-03 16:18:45 +0900

    timer 추가
    시펄

---
+++ LeagueOfLegendHeroes/timer.h
@@ -0,0 +1,32 @@
+#pragma once
+class timer
+{
+private:
+	bool			m_bIsHardware;
+	float			m_fTimeElapsed;
+	float			m_fTimeScale;
+
+	__int64			m_nCurTime;
+	__int64			m_nLastTime;
+	__int64			m_nPeriodFrequency;
+
+	unsigned long	m_dFrameRate;
+	unsigned long	m_dFPSFrameCount;
+
+	float			m_fFPSTimeElapsed;
+	float			m_fWorldTime;
+
+public:
+	void Setup();
+	void Release();
+	void tick(float lockFPS = 0.0f);
+
+	unsigned long GetFrameRate(char * str) const;
+
+	inline float	GetElapsedTime(void) const { return m_fTimeElapsed; }
+	inline float	GetWorldTime(void) const { return m_fWorldTime; }
+	
+	timer();
+	~timer();
+};
+
