7b728ea6b17f1e518e0188f3cf4123ef4e2233cb
DESKTOP-H544JN2\idhik <idhik@naver.com>
2017-09-03 15:01:33 +0900

    GetFrameRate 추가했다 빌드될꺼얌

--- LeagueOfLegendHeroes/timer.cpp
+++ LeagueOfLegendHeroes/timer.cpp
@@ -81,3 +81,9 @@ void timer::tick(float lockFPS)
 	}
 }
 
+unsigned long timer::GetFrameRate(char * str) const
+{
+	if (str != NULL) sprintf(str, "FPS : %d", m_dFrameRate);
+	return m_dFrameRate;
+}
+
