02387bc66d98fa9d2011abbeceb1ecd53fdf7327
DESKTOP-H544JN2\idhik <idhik@naver.com>
2017-09-04 09:09:42 +0900

    cMainGame 랜더부분 추가했습니다.

--- LeagueOfLegendHeroes/cMainGame.cpp
+++ LeagueOfLegendHeroes/cMainGame.cpp
@@ -23,4 +23,16 @@ void cMainGame::Update()
 
 void cMainGame::Render()
 {
+	g_pD3DDevice->Clear(NULL, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
+	g_pD3DDevice->BeginScene();
+	///// 여기서부터 렌더 시작
+
+
+
+
+
+
+	/////  렌더 끝
+	g_pD3DDevice->EndScene();
+	g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
 }
