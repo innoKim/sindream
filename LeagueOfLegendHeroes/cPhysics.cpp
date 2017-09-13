971eed8ba8aa6bd7197ea7532fcf67428b1029f2
DESKTOP-H544JN2\idhik <idhik@naver.com>
2017-09-14 08:57:08 +0900

    cPhysics 내용 추가

--- LeagueOfLegendHeroes/cPhysics.cpp
+++ LeagueOfLegendHeroes/cPhysics.cpp
@@ -6,7 +6,6 @@ cPhysics::cPhysics()
 {
 }
 
-
 cPhysics::~cPhysics()
 {
 }
@@ -21,4 +20,32 @@ void cPhysics::Update()
 	(*m_pvPos)		+= m_vVelocity;
 
 	m_fRestDuration += g_pTimeManager->GetElapsedTime();
+}
+
+bool cPhysics::ColisionWith(vector<cPhysics*> vecVersusObject)
+{
+	for (int i = 0; i < vecVersusObject.size(); i++)
+	{
+		if (m_fRestDuration > 0 || vecVersusObject[i]->GetRestDuration()>0) return;
+
+		SphereVsSphere(this, vecVersusObject[i]);
+	}	
+}
+
+void cPhysics::SphereVsSphere(cPhysics * Object1, cPhysics * Object2)
+{
+	D3DXVECTOR3 distanceVector = (*Object1->GetPositionPtr()) - (*Object2->GetPositionPtr());
+	float dist = D3DXVec3LengthSq(&distanceVector);
+
+	if (((Object1->GetRadius() + Object1->GetRadius()) / 2)*((Object1->GetRadius() + Object1->GetRadius()) / 2) > dist) return;
+	
+
+
+	Object1->SetRestDuration(0.0f);
+	Object2->SetRestDuration(0.0f);
+}
+
+void cPhysics::SphereVsGround()
+{
+	
 }
--- LeagueOfLegendHeroes/cPhysics.h
+++ LeagueOfLegendHeroes/cPhysics.h
@@ -1,19 +1,37 @@
 #pragma once
 
 #define PHYSICS_GRAVITY 0.01f
+//
+//struct ST_Contact
+//{
+//	ST_Contact() : Pn(0.0f), Pt(0.0f), Pnb(0.0f) {}
+//
+//	D3DXVECTOR3 position;
+//	D3DXVECTOR3 normal;
+//	D3DXVECTOR3 r1, r2;
+//	float separation;
+//	float Pn;	// accumulated normal impulse
+//	float Pt;	// accumulated tangent impulse
+//	float Pnb;	// accumulated normal impulse for position bias
+//	float massNormal, massTangent;
+//};
 
 class cPhysics
 {
 private:
 	bool m_bStatic; //충돌의 대상이 되지만 움직이지 않는 고정된놈 (무한의 질량) ex) 바닥, 벽
 
+	SYNTHESIZE_PASS_BY_REF(float, m_fRadius, Radius); // 지름
+
 	SYNTHESIZE_PASS_BY_REF(float,		m_fMass,			Mass); // 질량
 	SYNTHESIZE_PASS_BY_REF(D3DXVECTOR3, m_vAcceleration,	Acceleration); //가속도
 	SYNTHESIZE_PASS_BY_REF(D3DXVECTOR3, m_vVelocity,		Velovity);	//속도
 
-	SYNTHESIZE(D3DXVECTOR3*, m_pvPos, PositionPtr);
+	SYNTHESIZE(D3DXVECTOR3*,	m_pvPos,			PositionPtr);
+	SYNTHESIZE(float,			m_fRestDuration,	RestDuration);		//마지막 충돌로부터의 시간;
 
-	float m_fRestDuration; //마지막 충돌로부터의 시간;
+	void SphereVsSphere(cPhysics * Object1, cPhysics * Object2);
+	void SphereVsGround();
 
 public:
 	cPhysics();
@@ -21,5 +39,6 @@ public:
 
 	void Setup();
 	void Update();
+	bool ColisionWith(vector<cPhysics*> vecVersusObject);
 };
 
