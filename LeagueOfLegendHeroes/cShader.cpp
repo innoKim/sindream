a334a9ba31d09755cee8a37026394353ce9ab700
daeguny <whiteagl1004@gmail.com>
2017-09-13 16:59:54 +0900

    cShader 클래스 수정 섀도우 테스트 완료

--- LeagueOfLegendHeroes/cShader.cpp
+++ LeagueOfLegendHeroes/cShader.cpp
@@ -3,14 +3,20 @@
 
 
 cShader::cShader()
-	: m_pEffect(NULL)
+	: m_pApplyShadow(NULL)
+	, m_pCreateShadow(NULL)
 	, m_pMesh(NULL)
+	, m_pDisc(NULL)
 	, m_pDMTexture(NULL)
 	, m_pSMTexture(NULL)
 	, m_pNMTexture(NULL)
 	, m_pEnvTexture(NULL)
 	, m_pvCameraPos(NULL)
+	, m_pShadowRenderTarget(NULL)
 	, m_vLightColor(D3DXVECTOR4(0.7f, 0.7f, 1.0f, 1.0f))
+	, m_vTorusColor(D3DXVECTOR4(1, 1, 0, 1))
+	, m_vDiscColor(D3DXVECTOR4(0, 1, 1, 1))
+	, m_vLightPos(D3DXVECTOR4(-300.0f, 300.0f, -300.0f, 1.0f))
 	, m_sFolder("shader/")
 	, m_fRotationY(0.0f)
 {
@@ -21,10 +27,52 @@ cShader::~cShader()
 {
 	m_pvCameraPos = NULL;
 
-	SAFE_RELEASE(m_pEffect);
+	SAFE_RELEASE(m_pShadowRenderTarget);
+	SAFE_RELEASE(m_pEnvTexture);
+	SAFE_RELEASE(m_pApplyShadow);
+	SAFE_RELEASE(m_pCreateShadow);
 	SAFE_RELEASE(m_pMesh);
+	SAFE_RELEASE(m_pDisc);
+	SAFE_RELEASE(m_pShadowDepthStencil);
 	//SAFE_RELEASE(m_pDMTexture);
 	//SAFE_RELEASE(m_pSMTexture);
+}
+
+void cShader::SetupShadow(D3DXVECTOR3 * pvEye, char * szApplyFileName, char * szCreateFileName, char* szMeshFileName)
+{
+	string sFileName = szApplyFileName;
+
+	string sFullPath = m_sFolder + sFileName;
+
+	char* szFullPath = (char*)sFullPath.c_str();
+
+	m_pApplyShadow = LoadEffect(szFullPath);
+
+	sFileName = szCreateFileName;
+
+	sFullPath = m_sFolder + sFileName;
+
+	szFullPath = (char*)sFullPath.c_str();
+
+	m_pCreateShadow = LoadEffect(szFullPath);
+
+	sFileName = szMeshFileName;
+
+	sFullPath = m_sFolder + sFileName;
+
+	szFullPath = (char*)sFullPath.c_str();
+
+	D3DXLoadMeshFromX(szFullPath, D3DXMESH_MANAGED, g_pD3DDevice, NULL, NULL, NULL, NULL, &m_pMesh);
+
+	D3DXLoadMeshFromX("shader/disc.x", D3DXMESH_MANAGED, g_pD3DDevice, NULL, NULL, NULL, NULL, &m_pDisc);
+
+	m_pvCameraPos = pvEye;
+
+	const int shadowMapSize = 2048;
+
+	g_pD3DDevice->CreateTexture(shadowMapSize, shadowMapSize, 1, D3DUSAGE_RENDERTARGET, D3DFMT_R32F, D3DPOOL_DEFAULT, &m_pShadowRenderTarget, NULL);
+
+	g_pD3DDevice->CreateDepthStencilSurface(shadowMapSize, shadowMapSize, D3DFMT_D24X8, D3DMULTISAMPLE_NONE, 0, TRUE, &m_pShadowDepthStencil, NULL);
 }
 
 void cShader::Setup(D3DXVECTOR3* pvEye, char * szFxFileName, char * szMeshFileName, char* szDMTextureFileName, char* szSMTextureFileName, char* szNMTextureFileName, char* szEnvTextureFileName)
@@ -35,7 +83,7 @@ void cShader::Setup(D3DXVECTOR3* pvEye, char * szFxFileName, char * szMeshFileNa
 
 	char* szFullPath = (char*)sFullPath.c_str();
 
-	m_pEffect = LoadEffect(szFullPath);
+	m_pApplyShadow = LoadEffect(szFullPath);
 
 	sFileName = szMeshFileName;
 
@@ -90,86 +138,139 @@ void cShader::Setup(D3DXVECTOR3* pvEye, char * szFxFileName, char * szMeshFileNa
 	}
 
 	m_pvCameraPos = pvEye;
-	m_vLightPos = D3DXVECTOR4(-500.0f, 500.0f, -500.0f, 1.0f);
 }
 
 void cShader::Render()
 {
-	D3DXMATRIXA16 matS, matR, matWorld, matView, matProjection, matViewProjection, matWolrdViewProjection, matWorldInverse;
-
-	// 프레임마다 0.4도씩 회전을 시킨다.
-	//m_fRotationY += 0.4f * D3DX_PI / 180.0f;
-	//if (m_fRotationY > 2 * D3DX_PI)
-	//{
-	//	m_fRotationY -= 2 * D3DX_PI;
-	//}
-
-	//D3DXMatrixRotationX(&matR, -D3DX_PI / 2.0f);
-
-	D3DXMatrixIdentity(&matWorld);
-
-	//D3DXMatrixInverse(&matWorldInverse, 0, &matWorld);
-
-	g_pD3DDevice->GetTransform(D3DTS_VIEW, &matView);
-	g_pD3DDevice->GetTransform(D3DTS_PROJECTION, &matProjection);
-
-	matViewProjection = matView * matProjection;
-	matWolrdViewProjection = matWorld * matView * matProjection;
-
-	D3DXVECTOR4 vCameraPos = D3DXVECTOR4(m_pvCameraPos->x, m_pvCameraPos->y, m_pvCameraPos->z, 1.0f);
-	D3DXVECTOR4 vSurfaceColor = D3DXVECTOR4(0.7f, 0.7f, 1.0f, 1.0f);
-
-	// 쉐이더 전역변수들을 설정
-	m_pEffect->SetFloat("fWaveHeight", 3.0f);
-	m_pEffect->SetFloat("fSpeed", 2.0f);
-	m_pEffect->SetFloat("fWaveFrequency", 10.0f);
-	m_pEffect->SetFloat("fUVSpeed", 0.25f);
-
-	ULONGLONG tick = GetTickCount64();
-	m_pEffect->SetFloat("fTime", tick / 1000.0f);
-
-	m_pEffect->SetMatrix("matView", &matView);
-	m_pEffect->SetMatrix("matProjection", &matProjection);
-	m_pEffect->SetMatrix("matWorld", &matWorld);
-	//m_pEffect->SetMatrix("matWorldInverse", &matWorldInverse);
-	m_pEffect->SetVector("vLightPos", &m_vLightPos);
-	m_pEffect->SetVector("vCameraPos", &vCameraPos);
-	m_pEffect->SetVector("vLightColor", &vSurfaceColor);
-
-	if (m_pDMTexture)
+	//광원-뷰 행렬을 만든다
+	D3DXMATRIXA16 matLightView;
 	{
-		m_pEffect->SetTexture("DiffuseMap_Tex", m_pDMTexture);
+		D3DXVECTOR3 vEyePt(m_vLightPos.x, m_vLightPos.y, m_vLightPos.z);
+
+		D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
+
+		D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);
+
+		D3DXMatrixLookAtLH(&matLightView, &vEyePt, &vLookatPt, &vUpVec);
 	}
 
-	if (m_pSMTexture)
+	//광원-투영 행렬을 만든다
+	D3DXMATRIXA16 matLightProjection;
 	{
-		m_pEffect->SetTexture("SpecularMap_Tex", m_pSMTexture);
+		D3DXMatrixPerspectiveFovLH(&matLightProjection, D3DX_PI / 4.0f, 1, 1, 10000);
 	}
 
-	if (m_pNMTexture)
+	//뷰-투영 행렬을 만든다
+	D3DXMATRIXA16 matViewProjection;
 	{
-		m_pEffect->SetTexture("NormalMap_Tex", m_pNMTexture);
+		//뷰 행렬을 만든다
+		D3DXMATRIXA16 matView, matProjection;
+
+		g_pD3DDevice->GetTransform(D3DTS_VIEW, &matView);
+		g_pD3DDevice->GetTransform(D3DTS_PROJECTION, &matProjection);
+
+		matViewProjection = matView * matProjection;
 	}
 
-	if (m_pEnvTexture)
+	//월드 행렬을 만든다
+	D3DXMATRIXA16 matWorld;
 	{
-		m_pEffect->SetTexture("EnvironmentMap_Tex", m_pEnvTexture);
+		//프레임마다 0.4도씩 회전시킨다
+		m_fRotationY += 0.4f * D3DX_PI / 180.0f;
+		if (m_fRotationY > D3DX_PI * 2)
+		{
+			m_fRotationY -= D3DX_PI * 2;
+		}
+		D3DXMatrixRotationY(&matWorld, m_fRotationY);
 	}
 
+	//디스크의 월드 행렬을 만든다
+	D3DXMATRIXA16 matDiscWorld;
+	{
+		D3DXMATRIXA16 matS, matT;
+
+		D3DXMatrixScaling(&matS, 2, 2, 2);
+		D3DXMatrixTranslation(&matT, 0, -40, 0);
+
+		matDiscWorld = matS * matT;
+	}
+
+	//현재 하드웨어 백버퍼와 깊이버퍼
+	LPDIRECT3DSURFACE9 pHWBackBuffer = NULL;
+	LPDIRECT3DSURFACE9 pHWDepthStencilBuffer = NULL;
+
+	g_pD3DDevice->GetRenderTarget(0, &pHWBackBuffer);
+	g_pD3DDevice->GetDepthStencilSurface(&pHWDepthStencilBuffer);
+
+	// 1. 그림자 만들기
+	LPDIRECT3DSURFACE9 pShadowSurface = NULL;
+
+	if (SUCCEEDED(m_pShadowRenderTarget->GetSurfaceLevel(0, &pShadowSurface)))
+	{
+		g_pD3DDevice->SetRenderTarget(0, pShadowSurface);
+		SAFE_RELEASE(pShadowSurface);
+	}
+
+	g_pD3DDevice->SetDepthStencilSurface(m_pShadowDepthStencil);
+
+	g_pD3DDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(255, 255, 255), 1.0f, 0);
+
+	m_pCreateShadow->SetMatrix("matWorld", &matWorld);
+	m_pCreateShadow->SetMatrix("matLightView", &matLightView);
+	m_pCreateShadow->SetMatrix("matLightProjection", &matLightProjection);
+
+	m_pCreateShadow->SetVector("vLightPos", &m_vLightPos);
 
 	UINT numPasses = 0;
-	m_pEffect->Begin(&numPasses, NULL);
+	m_pCreateShadow->Begin(&numPasses, NULL);
 
-	for (UINT i = 0; i < numPasses; ++i)
+	for (int i = 0; i < numPasses; i++)
 	{
-		m_pEffect->BeginPass(i);
+		m_pCreateShadow->BeginPass(i);
 
 		m_pMesh->DrawSubset(0);
 
-		m_pEffect->EndPass();
+		m_pCreateShadow->EndPass();
 	}
 
-	m_pEffect->End();
+	m_pCreateShadow->End();
+
+	//2. 그림자 입히기
+	g_pD3DDevice->SetRenderTarget(0, pHWBackBuffer);
+	g_pD3DDevice->SetDepthStencilSurface(pHWDepthStencilBuffer);
+
+	SAFE_RELEASE(pHWBackBuffer);
+	SAFE_RELEASE(pHWDepthStencilBuffer);
+
+	m_pApplyShadow->SetMatrix("matWorld", &matWorld);
+	m_pApplyShadow->SetMatrix("matLightView", &matLightView);
+	m_pApplyShadow->SetMatrix("matLightProjection", &matLightProjection);
+	m_pApplyShadow->SetMatrix("matViewProjection", &matViewProjection);
+
+	m_pApplyShadow->SetVector("vLightPos", &m_vLightPos);
+	m_pApplyShadow->SetVector("vObjectColor", &m_vTorusColor);
+
+	m_pApplyShadow->SetTexture("ShadowMap_Tex", m_pShadowRenderTarget);
+
+	numPasses = 0;
+	m_pApplyShadow->Begin(&numPasses, NULL);
+
+	for (int i = 0; i < numPasses; i++)
+	{
+		m_pApplyShadow->BeginPass(i);
+
+		m_pMesh->DrawSubset(0);
+
+		m_pApplyShadow->SetMatrix("matWorld", &matDiscWorld);
+		m_pApplyShadow->SetVector("vObjectColor", &m_vDiscColor);
+
+		m_pApplyShadow->CommitChanges();
+		m_pDisc->DrawSubset(0);
+
+		m_pApplyShadow->EndPass();
+	}
+
+	m_pApplyShadow->End();
 }
 
 LPD3DXEFFECT cShader::LoadEffect(char * szFileName)
